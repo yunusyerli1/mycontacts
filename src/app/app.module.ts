ea90a50793666d08eb77ed4e1e1d0169f8b38b04
yunusyerli1 <yunus.yerli@gaboras.com>
2022-09-13 11:28:17 +0300

    header and 404 page completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
